6e9bbd08b3566b850f386d8ad5486901b31fd0ff
vulcandth <vulcandth@gmail.com>
2025-01-26 18:30:35 -0600

    Update version8 to version9 patch

--- include/patching/PatchVersion8to9.h
+++ include/patching/PatchVersion8to9.h
@@ -5,17 +5,12 @@
 
 namespace patchVersion8to9Namespace {
 	using namespace patchVersion8to9Namespace;
-	constexpr int NUM_KEY_ITEMS_V9 = 0x25;
-	const std::vector<int> unusedEventIndexesV9 = {
-		127, 128, 265, 477, 479, 481, 760, 829, 960, 2200,
-		2233, 2234, 2235, 2236, 2237, 2238, 2239, 2240, 2241, 2242,
-		2243, 2244, 2245, 2246, 2247, 2248, 2249, 2250, 2251, 2252,
-		2253, 2254, 2255, 2256, 2257, 2258, 2259, 2260, 2261, 2262,
-		2263, 2264, 2265, 2266, 2267, 2268, 2269, 2270, 2271, 2272,
-		2273, 2274, 2275, 2276, 2277, 2278, 2279, 2280, 2281, 2282,
-		2283, 2284, 2285, 2286, 2287, 2288, 2289, 2290, 2291, 2292,
-		2293, 2294, 2295, 2296, 2297, 2298, 2299, 2300, 2301, 2302
-	};
+	constexpr int NUM_KEY_ITEMS_V9 = 0x26;
+	constexpr uint16_t INVALID_EVENT_FLAG = -1;
+	constexpr int NUM_EVENTS = 0x8ff;
+
+	// Converts a version 7 event flag to a version 8 event flag
+	uint16_t mapV8EventFlagToV9(uint16_t v8);
 
 	// converts a version 8 key item to a version 8 key item
 	uint8_t mapV8KeyItemToV9(uint8_t v8);
