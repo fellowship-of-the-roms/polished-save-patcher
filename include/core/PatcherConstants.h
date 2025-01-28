da028faf3d57a35d80f894f225194038265f4d90
vulcandth <vulcandth@gmail.com>
2025-01-27 18:36:51 -0600

    0.1.0 Release

--- include/core/PatcherConstants.h
+++ include/core/PatcherConstants.h
@@ -3,7 +3,7 @@
 
 #include <cstdint> // Add this include directive
 
-#define EMSCRIPTEN_PATCHER_VERSION "0.1.0-beta"
+#define EMSCRIPTEN_PATCHER_VERSION "0.1.0"
 
 // SRAM constants
 constexpr uint16_t SRAM_BANK_SIZE = 0x2000;
