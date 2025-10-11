4fbf6da8bb54e0f7aa26a92cf8c8e75c6829d7a5
vulcandth <6394873+vulcandth@users.noreply.github.com>
2025-10-11 14:55:30 -0500

    Bump version to 1.1.0

--- include/core/PatcherConstants.h
+++ include/core/PatcherConstants.h
@@ -3,7 +3,7 @@
 
 #include <cstdint> // Add this include directive
 
-#define EMSCRIPTEN_PATCHER_VERSION "1.0.5"
+#define EMSCRIPTEN_PATCHER_VERSION "1.1.0"
 
 // SRAM constants
 constexpr uint16_t SRAM_BANK_SIZE = 0x2000;
